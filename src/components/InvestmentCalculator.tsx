cb6e4a27bc846341b3cd140f29b17db6ee581903
Humarr <74363540+Humarr@users.noreply.github.com>
2025-07-12 15:03:43 +0100

    update monthly interest rate to 40%

--- src/components/InvestmentCalculator.tsx
+++ src/components/InvestmentCalculator.tsx
@@ -1,11 +1,12 @@
 'use client'
 import { useEffect, useState } from "react"
-import CurrencyDropdown from "./CurrencyDropdown"
+// import CurrencyDropdown from "./CurrencyDropdown"
 
 export default function InvestmentCalculator() {
   const [investment, setInvestment] = useState(5000)
   const [months, setMonths] = useState(12)
-  const [currency, setCurrency] = useState("USD")
+  const currency = "USD"
+  // const [currency, setCurrency] = useState("USD")
   // const [currency, setCurrency] = useState("NGN")
   const [result, setResult] = useState<number | null>(null)
   const [loading, setLoading] = useState(false)
@@ -13,12 +14,13 @@ export default function InvestmentCalculator() {
 
   const monthOptions = [1, 3, 6, 12, 24, 36]
 
-  const formatValue = (value: number) =>
-    new Intl.NumberFormat("en-US", {
-      style: currency === "BTC" ? "decimal" : "currency",
-      currency: currency === "BTC" ? "USD" : currency,
-      minimumFractionDigits: currency === "BTC" ? 8 : 2,
-    }).format(value)
+  const formatValue = (value: number) => value.toLocaleString()
+  // const formatValue = (value: number) =>
+  //   new Intl.NumberFormat("en-US", {
+  //     style: currency === "BTC" ? "decimal" : "currency",
+  //     currency: currency === "BTC" ? "USD" : currency,
+  //     minimumFractionDigits: currency === "BTC" ? 8 : 2,
+  //   }).format(value)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -95,13 +97,13 @@ export default function InvestmentCalculator() {
         </div>
 
         {/* Currency Selector */}
-        <div className="flex justify-end mb-6 my-2">
+        {/* <div className="flex justify-end mb-6 my-2">
           <CurrencyDropdown selected={currency} onChange={setCurrency} />
-        </div>
+        </div> */}
 
         {/* Result */}
         <div className="flex justify-between items-center">
-          <h3 className="text-lg font-medium text-white">Total amount (capital + ROI)</h3>
+          <h3 className="text-lg font-medium text-white">Total amount <span className="hidden sm:inline">(capital + ROI)</span></h3>
           <div className="text-2xl font-bold text-white">
             {loading ? "Calculating..." : result !== null ? formatValue(result) : "--"}
           </div>
