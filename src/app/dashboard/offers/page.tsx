11e50b1655f86bb6220f1bd65122ca804d473fc3
Humarr <74363540+Humarr@users.noreply.github.com>
2025-07-20 08:41:07 +0100

    Add current user instance to dashboard/offers page

--- src/app/dashboard/offers/page.tsx
+++ src/app/dashboard/offers/page.tsx
@@ -1,13 +1,15 @@
 import Pricing from "@/components/Pricing";
+import { getCurrentUser } from "@/lib/getCurrentUser";
 
-export default function OffersPage() {
+export default async function OffersPage() {
+  const currentUser = await getCurrentUser()
     return (
       <div>
         <h1 className="text-2xl font-bold text-brand-slate-800 mb-2">OUR OFFERS</h1>
         {/* <div className="bg-brand-cream-100 p-6 rounded-lg shadow">
           <p>Our offers content will go here.</p>
         </div> */}
-        <Pricing external={true}/>
+        <Pricing currentUser={currentUser} external={true}/>
       </div>
     )
-  }
+}
