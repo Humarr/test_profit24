cd4a3bcc012c145c8af187aa87c73d1f6cdca6e5
Humarr <74363540+Humarr@users.noreply.github.com>
2025-07-30 07:52:21 +0100

    remove admin privileges from user

--- src/app/dashboard/users/[id]/settings/page.tsx
+++ src/app/dashboard/users/[id]/settings/page.tsx
@@ -1,17 +1,40 @@
 // app/user/settings/page.tsx
 "use client";
-import React from "react";
 import ProfileInfoCard from "@/components/settings/ProfileInfoCard";
 import ReferralInfoCard from "@/components/settings/ReferralInfoCard";
-import SubscriptionInfoCard from "@/components/settings/SubscriptionInfoCard";
 import SecurityCard from "@/components/settings/SecurityCard";
+import SubscriptionInfoCard from "@/components/settings/SubscriptionInfoCard";
 // import NotificationPreferencesCard from "@/components/settings/NotificationPreferencesCard";
-import {DangerZoneCard} from "@/components/settings/DangerZoneCard";
+import { DangerZoneCard } from "@/components/settings/DangerZoneCard";
+import { getCurrentUser } from "@/lib/getCurrentUser";
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+interface User {
+    name: string;
+    id: string;
+    email: string;
+    phone: string | null;
+    role: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
 
 export default function UserSettingsPage() {
     const params = useParams(); // ✅ Not a promise
     const id = params?.id as string;
+    const [user, setUser] = useState<User | null>(null);
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const res = await getCurrentUser();
+                setUser(res);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchUser();
+    }, [id]);
   return (
     <div className="max-w-3xl mx-auto p-6 space-y-8">
       <h1 className="text-3xl font-bold text-brand-slate-800 mb-4">Account Settings</h1>
@@ -21,7 +44,7 @@ export default function UserSettingsPage() {
       <SubscriptionInfoCard />
       <SecurityCard />
       {/* <NotificationPreferencesCard /> */}
-      <DangerZoneCard userId={id}/>
+      {user?.role === "admin" && <DangerZoneCard userId={id}/>}
     </div>
   );
 }
